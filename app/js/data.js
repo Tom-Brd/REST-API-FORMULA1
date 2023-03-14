dba6af23952fd4f447fb522343585008b67c81a0
Tom-Brd <bourlard.tom.94@gmail.com>
2023-03-15 00:36:06 +0100

    refactor js functions depending of url

---
+++ app/js/data.js
@@ -0,0 +1,105 @@
+const queryString = window.location.search;
+const url = window.location.href;
+let dataType = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.html'));
+const urlParams = new URLSearchParams(queryString);
+const dataId = urlParams.get('id');
+
+if (urlParams.has('id')) {
+    getSingleData(dataType, dataId);
+} else {
+    getAllData(dataType);
+}
+
+function handleData(dataType, data) {
+    switch (dataType) {
+        case 'drivers':
+            createDriver(data);
+            break;
+        case 'circuits':
+            createCircuit(data);
+            break;
+        case 'teams':
+            createTeam(data);
+            break;
+        default:
+            break;
+    }
+}
+
+function getAllData(dataType) {
+    fetch('http://localhost:8888/REST-API-FORMULA1/' + dataType)
+        .then(response => response.json())
+        .then(allData => {
+            allData.forEach(singleData => {
+                handleData(dataType, singleData)
+            })
+        .catch(error => console.error(error));
+    })
+}
+
+
+function getSingleData(dataType, dataId) {
+    fetch('http://localhost:8888/REST-API-FORMULA1/' + dataType + '/show/' + dataId)
+        .then(response => response.json())
+        .then(data => {
+            handleData(dataType, data)
+        })
+        .catch(error => console.error(error));
+}
+
+function createDriver(driver) {
+    let driversTable = document.getElementById("drivers__table--body");
+    let driversList = document.createElement("tr");
+    let driverListName = document.createElement('td');
+    driverListName.textContent = `${driver.name}`;
+    driversList.appendChild(driverListName);
+    let driverListNationality = document.createElement('td');
+    driverListNationality.textContent = `${driver.nationality}`;
+    driversList.appendChild(driverListNationality);
+    let driverListDateOfBirth = document.createElement('td');
+    driverListDateOfBirth.textContent = `${driver.dateOfBirth}`;
+    driversList.appendChild(driverListDateOfBirth);
+    let driverListTeam = document.createElement('td');
+    driverListTeam.textContent = `${driver.team_name}`;
+    driversList.appendChild(driverListTeam);
+    let driverListCarNumber = document.createElement('td');
+    driverListCarNumber.textContent = `${driver.carNumber}`;
+    driversList.appendChild(driverListCarNumber);
+    driversTable.appendChild(driversList);
+}
+
+function createCircuit(circuit) {
+    let circuitsTable = document.getElementById("circuits__table--body");
+    let circuitsList = document.createElement("tr");
+    let circuitListName = document.createElement('td');
+    circuitListName.textContent = `${circuit.name}`;
+    circuitsList.appendChild(circuitListName);
+    let circuitListCountry = document.createElement('td');
+    circuitListCountry.textContent = `${circuit.country}`;
+    circuitsList.appendChild(circuitListCountry);
+    let circuitListLength = document.createElement('td');
+    circuitListLength.textContent = `${circuit.length}`;
+    circuitsList.appendChild(circuitListLength);
+    let circuitListNumberOfTurns = document.createElement('td');
+    circuitListNumberOfTurns.textContent = `${circuit.numberOfTurns}`;
+    circuitsList.appendChild(circuitListNumberOfTurns);
+    circuitsTable.appendChild(circuitsList);
+}
+
+function createTeam(team) {
+    let teamsTable = document.getElementById("teams__table--body");
+    let teamsList = document.createElement("tr");
+    let teamListName = document.createElement('td');
+    teamListName.textContent = `${team.name}`;
+    teamsList.appendChild(teamListName);
+    let teamListCountry = document.createElement('td');
+    teamListCountry.textContent = `${team.country}`;
+    teamsList.appendChild(teamListCountry);
+    let teamListTeamPrincipal = document.createElement('td');
+    teamListTeamPrincipal.textContent = `${team.teamPrincipal}`;
+    teamsList.appendChild(teamListTeamPrincipal);
+    let teamListYearOfCreation = document.createElement('td');
+    teamListYearOfCreation.textContent = `${team.yearOfCreation}`;
+    teamsList.appendChild(teamListYearOfCreation);
+    teamsTable.appendChild(teamsList);
+}
